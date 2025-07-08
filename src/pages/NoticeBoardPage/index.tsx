4e79d87390b7e76e30b4aa3817b058fdc7f94f6d
hansolc <hsc080808@naver.com>
2025-07-08 13:57:14 +0900

    add details in board Card

--- src/pages/NoticeBoardPage/index.tsx
+++ src/pages/NoticeBoardPage/index.tsx
@@ -7,13 +7,15 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import useInView from "@hooks/useInView";
 import { useEffect, useRef } from "react";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 function NoticeBoardPage() {
   const targetRef = useRef<HTMLDivElement | null>(null);
   const { data, fetchNextPage, hasNextPage, isError, isFetchingNextPage } =
     useSearchPostsQuery();
   const { observe } = useInView({ actionInView: fetchNextPage });
-  console.log(data);
 
   useEffect(() => {
     if (targetRef.current) {
@@ -39,9 +41,32 @@ function NoticeBoardPage() {
             >
               <CardMedia component="img" image="/images/logo.png" alt="logo" />
               <CardContent>
-                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                <Typography gutterBottom variant="h5" className="line-clamp-1">
+                  {post.title}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{ color: "text.secondary" }}
+                  className="line-clamp-4 h-[100px]"
+                >
                   {post.body}
                 </Typography>
+                <div className="flex justify-between pt-4">
+                  <div className="flex gap-1 items-center">
+                    <AccountCircleIcon fontSize="small" />
+                    <Typography variant="subtitle2">
+                      by {post.userId}
+                    </Typography>
+                  </div>
+                  <div className="flex gap-1">
+                    <VisibilityIcon fontSize="small" />
+                    <Typography variant="subtitle2">{post.views}</Typography>
+                    <FavoriteIcon fontSize="small" />
+                    <Typography variant="subtitle2">
+                      {post.reactions.likes}
+                    </Typography>
+                  </div>
+                </div>
               </CardContent>
             </Grid>
           );
